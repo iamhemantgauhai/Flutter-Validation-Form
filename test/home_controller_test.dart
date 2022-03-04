cd1a3552594ffcae82679238fbee5ee6cb7ed4b1
Hemant Gauhai <hemant20@navgurukul.org>
2022-03-04 14:30:55 +0530

    fix: Add

--- test/home_controller_test.dart
+++ test/home_controller_test.dart
@@ -11,18 +11,22 @@ import 'variable.dart';
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
   Get.testMode = true;
-  provideMockedNetworkImages(() async {
-  testWidgets(
-    'Login Check',
-        (tester) async{
-      Variable.loginBinding.builder();
-      await tester.pumpWidget(Variable.getTheMaterialAppWrapper(const HomeView()));
-      final loginButton=find.byKey(LoginWidgets.loginKey);
-      final emailField=find.byKey(LoginWidgets.emailKey);
-      final password=find.byKey(LoginWidgets.passwordKey);
-      await tester.enterText(emailField, 'hemantgauhai@gmail.com');
-      await tester.enterText(password, '!@#123asdASD');
-      await tester.tap(loginButton);
+  provideMockedNetworkImages(
+    () async {
+      testWidgets(
+        'Login Check',
+        (tester) async {
+          Variable.loginBinding.builder();
+          await tester
+              .pumpWidget(Variable.getTheMaterialAppWrapper(const HomeView()));
+          final loginButton = find.byKey(LoginWidgets.loginKey);
+          final emailField = find.byKey(LoginWidgets.emailKey);
+          final password = find.byKey(LoginWidgets.passwordKey);
+          await tester.enterText(emailField, 'hemantgauhai@gmail.com');
+          await tester.enterText(password, '!@#123asdASD');
+          await tester.tap(loginButton);
+        },
+      );
     },
   );
-});}
+}
